ed9d69aba1ec5b57fcbe063d7b787a919b9feb14
Lavisha Vyas <165463124+Lavishavyas@users.noreply.github.com>
2025-09-28 12:47:18 +0530

    Add files via upload

---
+++ oops.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Teacher {
+  private:
+    double salary;
+
+  public:
+  //properties / attributes
+  string name;
+  string dept;
+  string subject;
+
+  //methods / member functions
+  void changeDept(string newDept) {
+    dept = newDept;
+  }
+
+  //setter
+  void setSalary(double s) {
+    salary = s;
+  }
+
+  //getter
+  double getSalary() {
+    return salary;
+  }
+  
+};
+
+
+int main () {
+    Teacher t1;
+    t1.name = "Shradha";
+    t1.subject = "C++";
+    t1.dept = "Computer Science";
+    t1.setSalary(25000);
+
+    cout<<t1.name<<endl;
+    cout<<t1.getSalary()<<endl;
+    return 0;
+}
