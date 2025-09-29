4490ff34e822427e41364f40a97c0ea3d25e4e50
Lavisha Vyas <165463124+Lavishavyas@users.noreply.github.com>
2025-09-29 19:35:19 +0530

    Add files via upload

--- oops.cpp
+++ oops.cpp
@@ -6,38 +6,85 @@ class Teacher {
   private:
     double salary;
 
+  // public:
+  //non parameterized
+    // Teacher() {
+    //   dept = "Computer Science";
+    // }
+
   public:
-  //properties / attributes
+   //properties / attributes
   string name;
   string dept;
   string subject;
+
+
+  //parameterized
+    Teacher(string name, string dept, string subject, double salary) {
+      this->name = name;
+      this->dept = dept;
+      this->subject = subject;
+      this->salary = salary;
+    }
+
+    //copy constructor
+    Teacher(Teacher &orgObj) {
+      cout<<"i am custom sopy constructor....\n"<<endl;
+      this->name = orgObj.name;
+      this->dept = orgObj.dept;
+      this->subject = orgObj.subject;
+      this->salary = orgObj.salary;
+
+    }
 
   //methods / member functions
   void changeDept(string newDept) {
     dept = newDept;
   }
 
-  //setter
-  void setSalary(double s) {
-    salary = s;
-  }
+  // //setter
+  // void setSalary(double s) {
+  //   salary = s;
+  // }
 
-  //getter
-  double getSalary() {
-    return salary;
+  // //getter
+  // double getSalary() {
+  //   return salary;
+  // }
+
+  void getInfo() {
+    cout<<"name : "<<name<<endl;
+    cout<<"subject : "<<subject<<endl; 
   }
   
 };
 
 
-int main () {
-    Teacher t1;
-    t1.name = "Shradha";
-    t1.subject = "C++";
-    t1.dept = "Computer Science";
-    t1.setSalary(25000);
+// class Acount {
+//   private:
+//   double balance;
+//   string password; //data hiding
 
-    cout<<t1.name<<endl;
-    cout<<t1.getSalary()<<endl;
+//   public:
+//   string accountId;
+//   string username;
+// };
+
+
+
+
+int main () {
+    Teacher t1("Shradha", "Computer Science", "C++", 25000); //constructor call
+    // t1.getInfo();
+
+    Teacher t2(t1); //custom copy constructor - invoke
+    t2.getInfo();
+
+
+    // t1.name = "Shradha";
+    // t1.subject = "C++";
+    // t1.setSalary(25000);
+
+    // cout<<t1.getSalary()<<endl;
     return 0;
 }
